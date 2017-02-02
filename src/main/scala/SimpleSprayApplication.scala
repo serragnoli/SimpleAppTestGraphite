6aad74968d2fb760e390d370d9937c081b542d2a
Fabio Serragnoli <fabio.serragnoli@sky.uk>
2017-02-02 16:14:47 +0000

    Added counters for Top Hits

--- src/main/scala/SimpleSprayApplication.scala
+++ src/main/scala/SimpleSprayApplication.scala
@@ -1,9 +1,11 @@
 import akka.actor.ActorSystem
 import akka.pattern.after
 import kamon.Kamon
+import kamon.metric.instrument.Counter
 import kamon.spray.KamonTraceDirectives
 import spray.routing.SimpleRoutingApp
 
+import scala.collection.mutable
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.language.postfixOps
@@ -15,28 +17,37 @@ object SimpleSprayApplication extends App with SimpleRoutingApp with KamonTraceD
   import system.dispatcher
 
   val someCounter = Kamon.metrics.counter("fabs-some-counter")
-  val someFailture = Kamon.metrics.instrumentFactory("TEst")
+  val top10Counter1 = Kamon.metrics.counter("top-10-counter")
+  val top10Counter = mutable.Map.empty[String, Counter]
+  val failure = Kamon.metrics.instrumentFactory("TEst")
 
   startServer("localhost", 9090, serviceActorName = "routing-actor") {
     get {
-      path("resource-a") {
-        traceName("CoolResourceA") {
-          complete {
-            someCounter.increment(5)
+      path("resource-a" / Segment) {
+        uuid =>
+          traceName("CoolResourceA") {
+            complete {
+              someCounter.increment(5)
+              for (_ <- 1 to 100000) {
+                List[Double](Math.random, Math.random, Math.random)
+              }
+              someCounter.increment()
+              top10Counter.get(uuid) match {
+                case None => top10Counter.put(uuid, Kamon.metrics.counter(s"movies.top10.$uuid"))
+                case Some(v) => v.increment()
+              }
 
-            for(_ <- 1 to 100000 ){
-              List[Double](Math.random, Math.random, Math.random)
+              println(s"Processed $uuid")
+
+              after(5 milliseconds, system.scheduler)(Future.successful("ok"))
             }
-
-            after(5 milliseconds, system.scheduler)(Future.successful("ok"))
           }
-        }
       }
     } ~
       path("resource-b") {
         traceName("SadResourceB") {
           complete {
-            for(_ <- 1 to 100000 ){
+            for (_ <- 1 to 100000) {
               List[Double](Math.random, Math.random, Math.random)
             }
 
